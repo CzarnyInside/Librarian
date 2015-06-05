b9d750307cebbeb5381c90fc66e9f35e4e1acb5a
Maciej Kozłowski <mkozlow1990@gmail.com>
2015-06-05 18:41:34 +0200

    change user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :books
 
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
